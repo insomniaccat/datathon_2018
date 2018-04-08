938031a767c7ec3175622d7eb1c8e14f4dd994a0
JustinClough <clougj@rpi.edu>
2018-04-07 23:28:53 -0400

    Fix file name formatting

--- model.py
+++ model.py
@@ -16,10 +16,10 @@ from sklearn.naive_bayes  import GaussianNB
 from sklearn.tree         import DecisionTreeClassifier
 
 # Define the file names
-pe_file_name = "Physician_Compare_2015_Group_Public_Reporting_-_Patient_Experience.csv"
+pe_file_name  = "Physician_Compare_2015_Group_Public_Reporting_-_Patient_Experience.csv"
 gps_file_name = "Physician_Compare_2015_Group_Public_Reporting___Performance_Scores.csv"
 ips_file_name = "Physician_Compare_2015_Individual_EP_Public_Reporting___Performance_Scores.csv"
-cn_file_name = "Physician_Compare_National_Downloadable_File.csv"
+cn_file_name  = "Physician_Compare_National_Downloadable_File.csv"
 
 # Read in the data
 pat_exp          = pn.read_csv( pe_file_name)
