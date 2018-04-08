d2678fd2c632b78f1c2e9d31e6c2508436fd841d
JustinClough <clougj@rpi.edu>
2018-04-07 23:14:58 -0400

    Corrects formatting for ML libs

--- model.py
+++ model.py
@@ -9,11 +9,11 @@ import seaborn           as sns
 from sklearn.linear_model import LogisticRegression
 from sklearn.linear_model import Perceptron
 from sklearn.linear_model import SGDClassifier
-from sklearn.svm import SVC, LinearSVC
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.naive_bayes import GaussianNB
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm          import SVC, LinearSVC
+from sklearn.ensemble     import RandomForestClassifier
+from sklearn.neighbors    import KNeighborsClassifier
+from sklearn.naive_bayes  import GaussianNB
+from sklearn.tree         import DecisionTreeClassifier
 
 # Define the file names
 pe_file_name = "Physician_Compare_2015_Group_Public_Reporting_-_Patient_Experience.csv"
@@ -26,3 +26,5 @@ pat_exp          = pn.read_csv( pe_file_name)
 group_perf_score = pn.read_csv( gps_file_name)
 indiv_perf_score = pn.read_csv( ips_file_name)
 phys_comp        = pn.read_csv( cn_file_name)
+
+
